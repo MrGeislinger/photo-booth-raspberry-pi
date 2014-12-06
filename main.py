b4b2e8f735fed8f030544d316b28d75c06931beb
Victor Geislinger <VictorLoren@users.noreply.github.com>
2014-12-06 07:10:51 -0800

    Uses custom 'camera.py' module in replace of the 'picamera' module   - Replaces custom camera functions in place of what was the 'picamera' function to help modularity.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
-import picamera
 import time
+#
+import camera
 
 # blinking function
 def blink(pin):
@@ -63,14 +64,6 @@ def countdownFrom(num,to=0,flashTime=1):
         flashNum(num,flashTime)
         num -= 1 # decrease the next number to show
 
-def takePicture(count):
-# Camera warm-up time
-    print "Picture Being Taken..."
-    camera.capture('foo%d.jpg' %count)
-    print "Picture was taken!!"
-    count += 1
-    return count
-
 
 #Press down button
 butPin = 12 # Broadcom pin 18 (P1 pin 12)
@@ -95,38 +88,27 @@ def buzzerOff():
 #count number of photos
 count = 0
 
-#
-print("Here we go! Press CTRL+C to exit")
-#Start camera
-camera = picamera.PiCamera()
-camera.vflip = True
-camera.start_preview()
-camera.resolution = (1024, 768)
+#Let user know how to stop
+print("Press CTRL+C to exit")
+
+#Start camera with given resolution and begin a preview window
+camera = camera.startCamera(1024, 768)
+camera.startPreview
 
 try:
     while True:
         input_state = GPIO.input(butPin)
         input_state_off = GPIO.input(butPinOff)
-        # Stop the camera!!!
+        #If stop button pressed, simulate the same as CTRL+C
         if input_state_off == False:
-            print 'endddd'
             raise KeyboardInterrupt('test')
+        #If camera button is pressed, count down to zero and buzz before taking a picture 
         if input_state == False:
             print('Button Pressed')
             countdownFrom(3)
             buzzerOn()
             time.sleep(0.2)
-            count = takePicture(count)
+            count = camera.takePicture(count)
             buzzerOff()
-        # else: # button is pressed:
-        #     print "test"
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
     GPIO.cleanup() # cleanup all GPIO
-
-
-
-# countdownFrom(10,flashTime=2.5)
-
-# GPIO.cleanup() 
-
-
