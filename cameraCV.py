ba3b153058686e6e425447a248461a67150c17ed
VictorLoren <VictorGeislinger+github@gmail.com>
2014-12-26 21:41:45 -0800

    Update cameraCV.py
      - Add methods to modify the video capture such as resolution at initialization.

--- cameraCV.py
+++ cameraCV.py
@@ -8,6 +8,14 @@ import cv2
 def startCamera(resX, resY, vflip=False, hflip=False, camNum=0):
 	#Get the video for the given camera
 	camera = cv2.VideoCapture(camNum)
+	#Set the resolutions
+	camera.set(CV_CAP_PROP_FRAME_WIDTH, resX)
+	camera.set(CV_CAP_PROP_FRAME_HEIGHT, resY)
+	return camera
+
+#Set the video frame
+def setFPS(camera, fps):
+	camera.set(CV_CAP_PROP_FRAME_WIDTH, fps)
 	return camera
 
 #Releasing the camera once done
